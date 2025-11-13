8596cf131d2c30e740d3c04c91c18e369ec06abe
nexus <122535095+breezly101@users.noreply.github.com>
2025-11-13 16:34:14 +0000

    changed bare

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://canlite.org/b/',
+    bare:'https://bare2.mysticmath.workers.dev',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
