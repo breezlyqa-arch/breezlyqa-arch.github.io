5bf3f7591ceb93f4ac5ec9c07fbf0058c449806c
nexus <122535095+breezly101@users.noreply.github.com>
2025-11-14 18:58:42 +0000

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://backend.infrared.bomberfish.ca',
+    bare:'https://infrared.zrhe2016.workers.dev',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
