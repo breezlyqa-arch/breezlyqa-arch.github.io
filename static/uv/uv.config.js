3696396dc0635b7928cca17045d3a227f328f245
nexus <122535095+breezly101@users.noreply.github.com>
2025-11-17 16:38:31 +0000

    bare change cause blocked

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://bare.koyeb.app',
+    bare:'https://mexi.mexi.rest/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
