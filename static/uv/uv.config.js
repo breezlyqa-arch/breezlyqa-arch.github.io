3f09849a572505004faf44b3632456a3572fa160
nexus <122535095+breezly101@users.noreply.github.com>
2025-11-17 00:37:15 +0000

    new bare

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://mexi.mexi.rest/bare/',
+    bare:'https://bare.koyeb.app',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
