02ad6cd68ec83c8e2e65e4a89010a74083999f22
nexus <122535095+breezly101@users.noreply.github.com>
2025-11-28 17:10:13 +0000

    /bare/

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://muddy-rice-ba72.acsessremote.workers.dev',
+    bare:'https://bare2.mysticmath.workers.dev',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
